38d2095a3549e05307d4ba07091945e03c5bae6f
eli-ennab <elinaennab@gmail.com>
2024-01-31 15:06:43 +0100

    globalStyle

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,4 @@
-import { container } from "@/vanilla_extract/app.css";
+import { container } from "@/vanilla_extract/app.css"
 
 export default function Home() {
   return <main className={container}></main>
--- vanilla_extract/app.css.ts
+++ vanilla_extract/app.css.ts
@@ -1,5 +1,11 @@
 import { style } from '@vanilla-extract/css'
+import { globalStyle } from '@vanilla-extract/css'
+
+globalStyle('html, body', {
+  margin: 0
+})
 
 export const container = style({
   backgroundColor: 'black',
+  height: '100vh'
 })

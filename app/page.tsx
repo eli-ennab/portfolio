f553f8f2342f9deac6798031c83f56e104135ca3
eli-ennab <elinaennab@gmail.com>
2024-02-21 11:21:49 +0100

    border >md

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ import { SideLinks } from './components/SideLinks'
 export default function Home() {
   return (
     <main className="tracking-widest font-main flex flex-col items-center">
-      <div className="frame flex flex-col p-12 gap-10 dark:border-white lg:justify-between border-solid border border-black">
+      <div className="frame flex flex-col p-12 gap-10 dark:border-white lg:justify-between md:border-solid md:border md:border-black">
         <Navigation />
         <SideLinks />
         <Footer />
